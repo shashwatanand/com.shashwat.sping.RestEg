c0edb11baf9e9ceeecbecef27763d70c3022ab02
Shashwat Anand <shashwat.anand@gmail.com>
2015-01-20 18:28:28 +0530

    Adding model classes

---
+++ src/main/java/com/shashwat/spring/controller/URIConstantsStudentRest.java
@@ -0,0 +1,9 @@
+package com.shashwat.spring.controller;
+
+public class URIConstantsStudentRest {
+	public static final String DUMMY_STUDENT = "/rest/student/dummy";
+    public static final String GET_STUDENT = "/rest/student/{id}";
+    public static final String GET_ALL_STUDENTS = "/rest/students";
+    public static final String CREATE_STUDENT = "/rest/student/create";
+    public static final String DELETE_STUDENT = "/rest/student/delete/{id}";
+}
---
+++ src/main/java/com/shashwat/spring/model/Student.java
@@ -0,0 +1,51 @@
+package com.shashwat.spring.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+
+public class Student implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String name;
+	private Date joiningDate;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the joiningDate
+	 */
+	@JsonSerialize(using = DateSerializer.class)
+	public Date getJoiningDate() {
+		return joiningDate;
+	}
+	/**
+	 * @param joiningDate the joiningDate to set
+	 */
+	public void setJoiningDate(Date joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+}
